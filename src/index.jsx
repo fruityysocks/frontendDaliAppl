453f3f3267271ac93c5c3255287f4de24586fe16
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-04-29 17:37:18 -0400

    Oops, accidentally deleted some code

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useParams } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
+import {
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router';
 import Counter from './store/components/counter';
 import Controls from './store/components/controls';
 
-function App() {
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+function FallBack(props) {
+  return <div> URL not found </div>;
+}
+
+function Nav(props) {
   return (
-    <div className="test">
-      <Counter />
-      <Controls />
-      All the REACT are belong to us!
-    </div>
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+      <div>
+        <h1>Counter</h1>
+        <Counter />
+        <Controls />
+      </div>
+    </BrowserRouter>
   );
 }
 
